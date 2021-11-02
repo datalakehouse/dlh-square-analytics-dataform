bf7c1f337952f4ad881f1ac971e880713de9f51d
guilhermealc <guilhermealc@hotmail.com>
2021-11-01 22:18:33 -0300

    First commit

--- includes/common.js
+++ includes/common.js
@@ -1,17 +1,13 @@
 /* 
  * The function countryGroup() takes as input the name of the country code field and returns a CASE statement that maps country codes to country groups
  * You can learn more about functions on https://docs.dataform.co/guides/includes
- */
+*/
 
-function countryGroup(countryCodeField) {
-  return `case
-            when lower(${countryCodeField}) in ('united states', 'canada') then 'NA'
-            when lower(${countryCodeField}) in ('united kingdom', 'france', 'germany', 'italy', 'poland') then 'EU'
-            when lower(${countryCodeField}) in ('australia') then lower(${countryCodeField})
-            else 'Other countries'
-            end`;
+function fullName(firstName, lastName) {
+  return `${firstName} || ' ' || ${lastName}`;
 }
 
 module.exports = {
-  countryGroup
+  fullName
 };
+ 
